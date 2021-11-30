8174b5222ceb0ee079405c59629e9a514d63bc03
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-11-30 17:18:10 +0300

    Init commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -3,5 +3,26 @@
  * на сервер.
  * */
 const createRequest = (options = {}) => {
-
+    const xhr = new XMLHttpRequest;
+    try {
+    	if (options.method === 'GET') {
+    		for (let key in options.data) {
+    			xhr.open(`${options.method}`, `${options.url}`?`${mail}`=`${options.data[key].email}`&`${password}`=`${options.data[key].password}`);
+    			xhr.send();
+    		}
+    	}
+    	else {
+    		const formData = new FormData();
+    		for (let key in options.data) {
+    			formData.append('mail', `${options.data[key].password}`);
+    			formData.append('password', `${options.data[key].password}`);
+    		}
+    		xhr.open(`${options.method}`, `${options.url}`);
+    		xhr.send(formData);
+    	}
+    	xhr.responseType = 'json';
+    	options.callback(null, xhr.response);
+    } catch (e) {
+    	options.callback(e, xhr.response);
+    }
 };
