17f5eab0fd97d8c3ca8bb56532b0686ce5e01ecc
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-12-07 19:55:41 +0300

    Init commit

--- public/js/ui/Modal.js
+++ public/js/ui/Modal.js
@@ -1,47 +1,54 @@
 /**
- * Класс Modal отвечает за
- * управление всплывающими окнами.
- * В первую очередь это открытие или
- * закрытие имеющихся окон
+ * Класс Modal отвечает за управление всплывающими окнами.
+ * В первую очередь это открытие или закрытие имеющихся окон.
  * */
 class Modal {
   /**
-   * Устанавливает текущий элемент в свойство element
-   * Регистрирует обработчики событий с помощью Modal.registerEvents()
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
+   * Устанавливает текущий элемент в свойство element.
+   * Регистрирует обработчики событий с помощью Modal.registerEvents().
+   * Если переданный элемент не существует, необходимо выкинуть ошибку.
    * */
   constructor(element){
-
+    this.element = element;
+    if (!element) {
+      throw new Error("Ошибка! Передан пустой элемент!");
+    }
+    this.registerEvents();
   }
 
   /**
-   * При нажатии на элемент с data-dismiss="modal"
-   * должен закрыть текущее окно
-   * (с помощью метода Modal.onClose)
+   * При нажатии на элемент с data-dismiss="modal" должен закрыть текущее окно (с помощью метода Modal.onClose).
    * */
   registerEvents() {
-
+    const modal = document.querySelectorAll("[data-dismiss]");
+    for (let i = 0; i < modal.length; i++) {
+      modal[i].addEventListener('click', (e) => {
+        e.preventDefault();
+        this.onClose();
+      }); 
+    }
   }
 
   /**
    * Срабатывает после нажатия на элементы, закрывающие окно.
-   * Закрывает текущее окно (Modal.close())
+   * Закрывает текущее окно (Modal.close()).
    * */
   onClose(e) {
-
+    this.close();
   }
+
   /**
-   * Открывает окно: устанавливает CSS-свойство display
-   * со значением «block»
+   * Открывает окно: устанавливает CSS-свойство display со значением «block».
    * */
   open() {
-
+    this.element.style.display = 'block';
   }
+
   /**
-   * Закрывает окно: удаляет CSS-свойство display
+   * Закрывает окно: удаляет CSS-свойство display.
    * */
   close(){
-
+    this.element.style.display = 'none';
   }
 }
+
--- public/js/ui/Sidebar.js
+++ public/js/ui/Sidebar.js
@@ -1,7 +1,6 @@
 /**
  * Класс Sidebar отвечает за работу боковой колонки:
- * кнопки скрытия/показа колонки в мобильной версии сайта
- * и за кнопки меню
+ * кнопки скрытия/показа колонки в мобильной версии сайта и за кнопки меню.
  * */
 class Sidebar {
   /**
@@ -13,22 +12,42 @@ class Sidebar {
   }
 
   /**
-   * Отвечает за скрытие/показа боковой колонки:
-   * переключает два класса для body: sidebar-open и sidebar-collapse
-   * при нажатии на кнопку .sidebar-toggle
+   * Отвечает за скрытие/показ боковой колонки:
+   * переключает два класса для body (sidebar-open и sidebar-collapse) при нажатии на кнопку .sidebar-toggle.
    * */
   static initToggleButton() {
-
+  	const sidebarMini = document.querySelector(".sidebar-mini");
+  	const sidebarToggle = document.querySelector(".sidebar-toggle");
+  	sidebarToggle.addEventListener('click', (e) => {
+  		e.preventDefault();
+  		sidebarMini.classList.toggle("sidebar-open"); 
+  		sidebarMini.classList.toggle("sidebar-collapse");
+  	});
   }
 
   /**
-   * При нажатии на кнопку входа, показывает окно входа
-   * (через найденное в App.getModal)
-   * При нажатии на кнопку регастрации показывает окно регистрации
-   * При нажатии на кнопку выхода вызывает User.logout и по успешному
-   * выходу устанавливает App.setState( 'init' )
+   * При нажатии на кнопку входа, показывает окно входа (через найденное в App.getModal).
+   * При нажатии на кнопку регистрации показывает окно регистрации.
+   * При нажатии на кнопку выхода вызывает User.logout и по успешному выходу устанавливает App.setState('init').
    * */
   static initAuthLinks() {
+  	const menuItemRegister = document.querySelector(".menu-item_register");
+  	const menuItemLogin = document.querySelector(".menu-item_login");
+  	const menuItemLogout = document.querySelector(".menu-item_logout");
 
+  	menuItemRegister.addEventListener('click', (e) => {
+  		e.preventDefault();
+  		App.getModal('register').open();
+  	});
+  	menuItemLogin.addEventListener('click', (e) => {
+  		e.preventDefault();
+  		App.getModal('login').open();
+  	});
+  	menuItemLogout.addEventListener('click', (e) => {
+  		e.preventDefault();
+  		User.logout();
+  		App.setState('init');
+  	});
   }
 }
+
