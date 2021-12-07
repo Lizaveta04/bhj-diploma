02108788d8d77ca283333fdd218513df048bc063
Elizaveta Kuzmicheva <kuzm_el@mail.ru>
2021-12-07 18:08:14 +0300

    Second commit

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,28 +1,25 @@
 /**
- * Основная функция для совершения запросов
- * на сервер.
+ * Основная функция для совершения запросов на сервер.
  * */
 const createRequest = (options = {}) => {
     const xhr = new XMLHttpRequest;
     try {
     	if (options.method === 'GET') {
-    		for (let key in options.data) {
-    			xhr.open(`${options.method}`, `${options.url}`?`${mail}`=`${options.data[key].email}`&`${password}`=`${options.data[key].password}`);
-    			xhr.send();
-    		}
-    	}
-    	else {
+    		xhr.open(`${options.method}`, `${options.url}?${Object.keys(options.data)[0]}=${Object.values(options.data)[0]}&${Object.keys(options.data)[1]}=${Object.values(options.data)[1]}`);
+    		xhr.send();
+    	} else {
     		const formData = new FormData();
-    		for (let key in options.data) {
-    			formData.append('mail', `${options.data[key].password}`);
-    			formData.append('password', `${options.data[key].password}`);
-    		}
+    		formData.append(`${Object.keys(options.data)[0]}`, `${Object.values(options.data)[0]}`);
+    		formData.append(`${Object.keys(options.data)[1]}`, `${Object.values(options.data)[1]}`);
     		xhr.open(`${options.method}`, `${options.url}`);
     		xhr.send(formData);
     	}
     	xhr.responseType = 'json';
-    	options.callback(null, xhr.response);
-    } catch (e) {
-    	options.callback(e, xhr.response);
+    	if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+    		options.callback(null, xhr.response);
+    	}
+    } catch (err) {
+    	options.callback(err);
     }
 };
+
